391a41475878c0f67ec46dfb6fc62f815c20f609
Emas87 <emanu.87@gmail.com>
2016-10-18 20:40:16 -0600

    Constructor done
    constructor completed

--- Semaforo.h
+++ Semaforo.h
@@ -12,17 +12,30 @@ class Semaforo {
    int Wait();	// Puede llamarse P
    private:
    int id;		// Identificador del semaforo
-
+   struct sembuf sbuf;
 };
 
 Semaforo::Semaforo(int ValorInicial = 0 ){
-Lab3ff
 	id = semget(KEY,1, 0775 | IPC_CREAT);
 
-	if(id < 0){
-      		cout << "No se pudo generar el semaforo.\n"<< strerror(id) << endl;
-      		exit(0);
+	if(id != 1){
+      	/* Initialize the semaphore. */
+        sbuf.sem_num = 0;
+        sbuf.sem_op = 2;  /* This is the number of runs
+                             without queuing. */
+        sbuf.sem_flg = 0;
+        if (semop(semid, &sbuf, 1) == -1) {
+            perror("IPC error: semop"); exit(1);
+        }
    	}
+	else if (errno == EEXIST) {
+        if ((semid = semget(semkey, 0, 0)) == -1) {
+            perror("IPC error 1: semget"); exit(1);
+        }
+    }
+    else {
+        perror("IPC error 2: semget"); exit(1);
+    }
 }
 
 Semaforo::Wait() {
