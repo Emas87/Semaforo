beab14dbab4730c915a6a4131ad1ff9310350e99
Emas87 <emanu.87@gmail.com>
2016-10-25 14:27:04 -0600

    main
    almost finished

--- Semaforo.h
+++ Semaforo.h
@@ -1,27 +1,30 @@
 #define KEY 0xA12345	// Valor de la llave del recurso
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/semaphore.h>
-#include <iostream.h>
+// #include <semaphore.h>
+#include <sys/sem.h>
+#include <iostream>
+#include <cstdlib>
 #include <errno.h>
 
+using namespace std;
 class Semaforo {
-   Semaforo( int ValorInicial = 0 );
-   ~Semaforo();
-   int Signal();	// Puede llamarse V
-   int Wait();	// Puede llamarse P
+   public:
+      Semaforo( int ValorInicial);
+      ~Semaforo(){ semctl(id, 0, 2, IPC_RMID); };
+      int Signal();	// Puede llamarse V
+      int Wait();	// Puede llamarse P
    private:
-   int id;		// Identificador del semaforo
-   struct sembuf sbuf;
+      int id;		// Identificador del semaforo
+      struct sembuf sbuf;
 };
 
 Semaforo::Semaforo(int ValorInicial = 0 ){
-	id = semget(KEY,1, 0775 | IPC_CREAT);
-	semun init;
-	init.val = ValorInicial;
-	semctl(id, 1, SETVAL, init);
-	if(id != 1){
-      	/* Initialize the semaphore. */
+	if(id = semget(KEY,1, 0775 | IPC_CREAT) < 0){
+      printf("Can\'t get id\n");
+      exit(-1);
+   } else if (id != -1){
+   
         sbuf.sem_num = 0;
         sbuf.sem_op = ValorInicial;  /* This is the number of runs
                              without queuing. */
@@ -29,18 +32,17 @@ Semaforo::Semaforo(int ValorInicial = 0 ){
         if (semop(id, &sbuf, 1) == -1) {
             perror("IPC error: semop"); exit(1);
         }
-   	}
-	else if (errno == EEXIST) {
+   } else if (errno == EEXIST) {
         if ((id = semget(KEY, 0, 0)) == -1) {
             perror("IPC error 1: semget"); exit(1);
         }
-    }
-    else {
+    } else {
+       cout << "id: " << id << endl;
         perror("IPC error 2: semget"); exit(1);
     }
 }
 
-Semaforo::Wait() {
+int Semaforo::Wait() {
 	sbuf.sem_num = 0;
 	sbuf.sem_op = -1;  /* This is the number of runs without queuing. */
 	sbuf.sem_flg = 0;
@@ -49,7 +51,7 @@ Semaforo::Wait() {
 	}
 }
 
-Semaforo::Signal() {
+int Semaforo::Signal() {
 	sbuf.sem_num = 0;
 	sbuf.sem_op = 1;  /* This is the number of runs without queuing. */
 	sbuf.sem_flg = 0;
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "Semaforo.h"
+
+int main() {
+    Semaforo s;
+    int j;
+
+    if ( fork() ) {
+       for (int i = 0; i < 10; i++ )
+           printf( "Esperando para activar el semaforo %d \n", i );
+       scanf( "%j", &j );
+       s.Signal();
+    }
+    else {
+       printf( "Esperando que el semaforo se active  ...\n" );
+       s.Wait();
+       printf( "Semaforo activado\n" );
+    }
+}
